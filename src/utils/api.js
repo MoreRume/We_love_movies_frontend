a8067602fe46084d9ed1616a9af1c2a17115d172
Rume <Sprtrks@gmail.com>
2024-04-30 19:58:41 -0400

    too many movies in url

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,5 +1,5 @@
 const API_BASE_URL =
-  process.env.API_BASE_URL || "https://we-love-movies-backend-bugu.onrender.com/movies";
+  process.env.API_BASE_URL || "https://we-love-movies-backend-bugu.onrender.com";
 
 /**
  * Defines the default headers for these functions to work with `json-server`
